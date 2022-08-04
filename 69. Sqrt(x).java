e2e4df41d6b880a280d5acfd8929887ad27ef4b0
Deep-0 <81347451+Deep-0@users.noreply.github.com>
2022-08-04 09:56:23 +0530

    Create 69. Sqrt(x).java

---
+++ 69. Sqrt(x).java
@@ -0,0 +1,18 @@
+class Solution {
+public int mySqrt(int x) {
+  int s = 0;
+  int e = x;
+  while (s <= e) {
+   long mid = s + (e - s) / 2;
+   if (mid * mid == x) {
+    return (int) mid;
+   }
+   if (mid * mid > x) {
+    e = (int) (mid - 1);
+   } else {
+    s = (int) (mid + 1);
+   }
+  }
+  return s - 1;
+ }
+}
